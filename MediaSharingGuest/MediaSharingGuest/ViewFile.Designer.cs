4b604c41d5997dd599884e56ce3da505a279f954
BartvanKeersop <bart.van.keersop@gmail.com>
2015-04-08 11:55:12 +0200

    another gui

---
+++ MediaSharingGuest/MediaSharingGuest/ViewFile.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace MediaSharingGuest
+{
+    partial class ViewFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReportFile = new System.Windows.Forms.Button();
+            this.btnReportComment = new System.Windows.Forms.Button();
+            this.btnLikeComment = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAddComment = new System.Windows.Forms.Button();
+            this.btnLikeThisFile = new System.Windows.Forms.Button();
+            this.lblLiked = new System.Windows.Forms.Label();
+            this.lblYourComment = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblComments = new System.Windows.Forms.Label();
+            this.tbComments = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReportFile
+            // 
+            this.btnReportFile.Location = new System.Drawing.Point(336, 647);
+            this.btnReportFile.Name = "btnReportFile";
+            this.btnReportFile.Size = new System.Drawing.Size(116, 40);
+            this.btnReportFile.TabIndex = 27;
+            this.btnReportFile.Text = "Report this file";
+            this.btnReportFile.UseVisualStyleBackColor = true;
+            // 
+            // btnReportComment
+            // 
+            this.btnReportComment.Location = new System.Drawing.Point(662, 647);
+            this.btnReportComment.Name = "btnReportComment";
+            this.btnReportComment.Size = new System.Drawing.Size(146, 40);
+            this.btnReportComment.TabIndex = 26;
+            this.btnReportComment.Text = "Report selected comment";
+            this.btnReportComment.UseVisualStyleBackColor = true;
+            // 
+            // btnLikeComment
+            // 
+            this.btnLikeComment.Location = new System.Drawing.Point(469, 647);
+            this.btnLikeComment.Name = "btnLikeComment";
+            this.btnLikeComment.Size = new System.Drawing.Size(146, 40);
+            this.btnLikeComment.TabIndex = 25;
+            this.btnLikeComment.Text = "Like selected comment!";
+            this.btnLikeComment.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(134, 647);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(116, 40);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAddComment
+            // 
+            this.btnAddComment.Location = new System.Drawing.Point(12, 647);
+            this.btnAddComment.Name = "btnAddComment";
+            this.btnAddComment.Size = new System.Drawing.Size(116, 40);
+            this.btnAddComment.TabIndex = 23;
+            this.btnAddComment.Text = "Add Comment";
+            this.btnAddComment.UseVisualStyleBackColor = true;
+            // 
+            // btnLikeThisFile
+            // 
+            this.btnLikeThisFile.Location = new System.Drawing.Point(377, 488);
+            this.btnLikeThisFile.Name = "btnLikeThisFile";
+            this.btnLikeThisFile.Size = new System.Drawing.Size(75, 52);
+            this.btnLikeThisFile.TabIndex = 22;
+            this.btnLikeThisFile.Text = "Like this file!";
+            this.btnLikeThisFile.UseVisualStyleBackColor = true;
+            // 
+            // lblLiked
+            // 
+            this.lblLiked.AutoSize = true;
+            this.lblLiked.Location = new System.Drawing.Point(12, 498);
+            this.lblLiked.Name = "lblLiked";
+            this.lblLiked.Size = new System.Drawing.Size(36, 13);
+            this.lblLiked.TabIndex = 21;
+            this.lblLiked.Text = "Liked:";
+            // 
+            // lblYourComment
+            // 
+            this.lblYourComment.AutoSize = true;
+            this.lblYourComment.Location = new System.Drawing.Point(12, 530);
+            this.lblYourComment.Name = "lblYourComment";
+            this.lblYourComment.Size = new System.Drawing.Size(78, 13);
+            this.lblYourComment.TabIndex = 20;
+            this.lblYourComment.Text = "Your comment:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 546);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(440, 95);
+            this.textBox2.TabIndex = 19;
+            // 
+            // lblComments
+            // 
+            this.lblComments.AutoSize = true;
+            this.lblComments.Location = new System.Drawing.Point(466, 14);
+            this.lblComments.Name = "lblComments";
+            this.lblComments.Size = new System.Drawing.Size(59, 13);
+            this.lblComments.TabIndex = 18;
+            this.lblComments.Text = "Comments:";
+            // 
+            // tbComments
+            // 
+            this.tbComments.Location = new System.Drawing.Point(469, 30);
+            this.tbComments.Multiline = true;
+            this.tbComments.Name = "tbComments";
+            this.tbComments.Size = new System.Drawing.Size(339, 611);
+            this.tbComments.TabIndex = 17;
+            this.tbComments.Text = "Comments";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 364);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(443, 122);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "Description";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(12, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(27, 13);
+            this.lblTitle.TabIndex = 15;
+            this.lblTitle.Text = "Title";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(443, 330);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ViewFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 704);
+            this.Controls.Add(this.btnReportFile);
+            this.Controls.Add(this.btnReportComment);
+            this.Controls.Add(this.btnLikeComment);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAddComment);
+            this.Controls.Add(this.btnLikeThisFile);
+            this.Controls.Add(this.lblLiked);
+            this.Controls.Add(this.lblYourComment);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblComments);
+            this.Controls.Add(this.tbComments);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ViewFile";
+            this.Text = "ViewFile";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReportFile;
+        private System.Windows.Forms.Button btnReportComment;
+        private System.Windows.Forms.Button btnLikeComment;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnAddComment;
+        private System.Windows.Forms.Button btnLikeThisFile;
+        private System.Windows.Forms.Label lblLiked;
+        private System.Windows.Forms.Label lblYourComment;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblComments;
+        private System.Windows.Forms.TextBox tbComments;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
