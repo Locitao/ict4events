755d3afd72d58975191feac26daac8cd0680ab73
Locitao <rvdtera@gmail.com>
2015-04-15 10:24:50 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	ReservationApp/ReservationApp.v12.suo

--- ReservationApp/ReservationApp/Material.cs
+++ ReservationApp/ReservationApp/Material.cs
@@ -8,19 +8,33 @@ namespace ReservationApp
 {
     class Material
     {
+        /// <summary>
+        /// This class is for identifying the materials, so we don't work with
+        /// database objects in the program, but with objects.
+        /// First some auto-properties.
+        /// </summary>
         public string Name { get; set; }
 
         public decimal Price { get; set; }
 
         public int MaterialId { get; set; }
 
+        /// <summary>
+        /// Constructor for materials.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        /// <param name="materialId"></param>
         public Material(string name, decimal price, int materialId)
         {
             Name = name;
             Price = price;
             MaterialId = materialId;
         }
-
+        /// <summary>
+        /// ToString method to be able to display the materials in a listbox.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             string price = Convert.ToString(Price);
--- ReservationApp/ReservationApp/ReserveMaterials.cs
+++ ReservationApp/ReservationApp/ReserveMaterials.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace ReservationApp
 {
+    /// <summary>
+    /// This form will be used to reserve materials by the guest.
+    /// </summary>
     public partial class ReserveMaterials : Form
     {
         Select select = new Select();
@@ -32,21 +35,21 @@ namespace ReservationApp
 
         }
 
+        /// <summary>
+        /// FillList() fills the list of materials with materials
+        /// that haven't been reserved yet.
+        /// </summary>
         public void FillList()
         {
-            string name;
-            decimal price;
-            int id;
-
             try
             {
                 var materials = select.Select_Materials();
 
                 foreach (Dictionary<string, object> row in materials)
                 {
-                    name = Convert.ToString(row["MAT_NAME"]);
-                    price = Convert.ToDecimal(row["PRICE"]);
-                    id = Convert.ToInt32(row["MATERIAL_ID"]);
+                    var name = Convert.ToString(row["MAT_NAME"]);
+                    var price = Convert.ToDecimal(row["PRICE"]);
+                    var id = Convert.ToInt32(row["MATERIAL_ID"]);
 
                     allMaterials.Add(new Material(name, price, id));
                 }
@@ -56,7 +59,9 @@ namespace ReservationApp
                 MessageBox.Show(ex.Message);
             }
         }
-
+        /// <summary>
+        /// FillBox() fills the listbox with the materials in the list of materials.
+        /// </summary>
         public void FillBox()
         {
             foreach (Material m in allMaterials)
