517b352d273010c33e311e608b4047b78dc10cc0
martiener1 <martijnwillems34@hotmail.com>
2015-04-17 10:54:06 +0200

    Changes on material renting

--- Management System/Management System/MaterialRentingForm.cs
+++ Management System/Management System/MaterialRentingForm.cs
@@ -12,15 +12,36 @@ namespace Management_System
 {
     public partial class MaterialRentingForm : Form
     {
+        DatabaseConnection connection;
+        List<Material> materialList;
         public MaterialRentingForm()
         {
             InitializeComponent();
+            connection = new DatabaseConnection();
+            materialList = new List<Material>();
         }
 
 
         public void RefreshMaterialList()
         {
-
+            materialList.Clear();
+            lbMaterials.Items.Clear();
+            List<List<string>> output;
+            Exception exception;
+            if (connection.SQLQueryWithOutput("", out output, out exception))
+            {
+                foreach (List<string> list in output)
+                {
+                    Material tempMaterial = new Material();
+                    materialList.Add(tempMaterial);
+                    lbMaterials.Items.Add(tempMaterial);
+                }
+            }
+            else
+            {
+                MessageBox.Show("This error occured:" + Environment.NewLine + exception.ToString());
+            }
+            lbMaterials.SelectedIndex = 0;
         }
     }
 }
