c8cf1c4c3421d84c9e0fc4a95ed36ec905966345
Martijn Willems DESKTOP <martijn.willems@student.fontys.nl>
2015-04-22 19:27:50 +0200

    started on create account form

---
+++ Management System/Management System/CreateAccountForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Management_System
+{
+    partial class CreateAccountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lblRepeatPassword = new System.Windows.Forms.Label();
+            this.tbRepeatPassword = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(24, 57);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 9;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(24, 31);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(58, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Username:";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(109, 54);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 7;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(109, 28);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 6;
+            // 
+            // lblRepeatPassword
+            // 
+            this.lblRepeatPassword.AutoSize = true;
+            this.lblRepeatPassword.Location = new System.Drawing.Point(24, 83);
+            this.lblRepeatPassword.Name = "lblRepeatPassword";
+            this.lblRepeatPassword.Size = new System.Drawing.Size(94, 13);
+            this.lblRepeatPassword.TabIndex = 11;
+            this.lblRepeatPassword.Text = "Repeat Password:";
+            // 
+            // tbRepeatPassword
+            // 
+            this.tbRepeatPassword.Location = new System.Drawing.Point(109, 80);
+            this.tbRepeatPassword.Name = "tbRepeatPassword";
+            this.tbRepeatPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbRepeatPassword.TabIndex = 10;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(154, 107);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(54, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(27, 107);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(121, 23);
+            this.btnCreateAccount.TabIndex = 13;
+            this.btnCreateAccount.Text = "Create New Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // CreateAccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblRepeatPassword);
+            this.Controls.Add(this.tbRepeatPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbName);
+            this.Name = "CreateAccountForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label lblRepeatPassword;
+        private System.Windows.Forms.TextBox tbRepeatPassword;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnCreateAccount;
+    }
+}
---
+++ Management System/Management System/CreateAccountForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Management_System
+{
+    public partial class CreateAccountForm : Form
+    {
+        public bool saved;
+        public CreateAccountForm()
+        {
+            InitializeComponent();
+            saved = false;
+            connection = new DatabaseConnection();
+        }
+
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            if (lblPassword.Text == lblRepeatPassword.Text)
+            {
+                saved = true;
+                this.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
