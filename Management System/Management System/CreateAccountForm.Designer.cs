38736053a578e9d1520fb33ccf2e5fe9fa73828a
martiener1 <martijnwillems34@hotmail.com>
2015-04-28 10:10:30 +0200

    creating an account now requires more input.

--- Management System/Management System/CreateAccountForm.Designer.cs
+++ Management System/Management System/CreateAccountForm.Designer.cs
@@ -36,6 +36,14 @@
             this.tbRepeatPassword = new System.Windows.Forms.TextBox();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.lblRights = new System.Windows.Forms.Label();
+            this.cbRights = new System.Windows.Forms.ComboBox();
+            this.lbPhoneNumber = new System.Windows.Forms.Label();
+            this.lblEmployeeName = new System.Windows.Forms.Label();
+            this.tbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.tbEmployeeName = new System.Windows.Forms.TextBox();
+            this.lblAdress = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblPassword
@@ -60,7 +68,7 @@
             // 
             this.tbPassword.Location = new System.Drawing.Point(124, 54);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.Size = new System.Drawing.Size(134, 20);
             this.tbPassword.TabIndex = 7;
             this.tbPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress_LettersAndDigits);
             // 
@@ -68,7 +76,7 @@
             // 
             this.tbName.Location = new System.Drawing.Point(124, 28);
             this.tbName.Name = "tbName";
-            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.Size = new System.Drawing.Size(134, 20);
             this.tbName.TabIndex = 6;
             this.tbName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress_LettersAndDigits);
             // 
@@ -85,13 +93,13 @@
             // 
             this.tbRepeatPassword.Location = new System.Drawing.Point(124, 80);
             this.tbRepeatPassword.Name = "tbRepeatPassword";
-            this.tbRepeatPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbRepeatPassword.Size = new System.Drawing.Size(134, 20);
             this.tbRepeatPassword.TabIndex = 10;
             this.tbRepeatPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_KeyPress_LettersAndDigits);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(154, 107);
+            this.btnCancel.Location = new System.Drawing.Point(154, 246);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(70, 23);
             this.btnCancel.TabIndex = 12;
@@ -101,7 +109,7 @@
             // 
             // btnCreateAccount
             // 
-            this.btnCreateAccount.Location = new System.Drawing.Point(27, 107);
+            this.btnCreateAccount.Location = new System.Drawing.Point(27, 227);
             this.btnCreateAccount.Name = "btnCreateAccount";
             this.btnCreateAccount.Size = new System.Drawing.Size(121, 23);
             this.btnCreateAccount.TabIndex = 13;
@@ -109,11 +117,84 @@
             this.btnCreateAccount.UseVisualStyleBackColor = true;
             this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
             // 
+            // lblRights
+            // 
+            this.lblRights.AutoSize = true;
+            this.lblRights.Location = new System.Drawing.Point(23, 187);
+            this.lblRights.Name = "lblRights";
+            this.lblRights.Size = new System.Drawing.Size(40, 13);
+            this.lblRights.TabIndex = 26;
+            this.lblRights.Text = "Rights:";
+            // 
+            // cbRights
+            // 
+            this.cbRights.FormattingEnabled = true;
+            this.cbRights.Location = new System.Drawing.Point(124, 184);
+            this.cbRights.Name = "cbRights";
+            this.cbRights.Size = new System.Drawing.Size(134, 21);
+            this.cbRights.TabIndex = 25;
+            // 
+            // lbPhoneNumber
+            // 
+            this.lbPhoneNumber.AutoSize = true;
+            this.lbPhoneNumber.Location = new System.Drawing.Point(24, 161);
+            this.lbPhoneNumber.Name = "lbPhoneNumber";
+            this.lbPhoneNumber.Size = new System.Drawing.Size(79, 13);
+            this.lbPhoneNumber.TabIndex = 24;
+            this.lbPhoneNumber.Text = "Phone number:";
+            // 
+            // lblEmployeeName
+            // 
+            this.lblEmployeeName.AutoSize = true;
+            this.lblEmployeeName.Location = new System.Drawing.Point(24, 109);
+            this.lblEmployeeName.Name = "lblEmployeeName";
+            this.lblEmployeeName.Size = new System.Drawing.Size(38, 13);
+            this.lblEmployeeName.TabIndex = 23;
+            this.lblEmployeeName.Text = "Name:";
+            // 
+            // tbPhoneNumber
+            // 
+            this.tbPhoneNumber.Location = new System.Drawing.Point(124, 158);
+            this.tbPhoneNumber.Name = "tbPhoneNumber";
+            this.tbPhoneNumber.Size = new System.Drawing.Size(134, 20);
+            this.tbPhoneNumber.TabIndex = 22;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(124, 135);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(134, 20);
+            this.tbAddress.TabIndex = 21;
+            // 
+            // tbEmployeeName
+            // 
+            this.tbEmployeeName.Location = new System.Drawing.Point(124, 109);
+            this.tbEmployeeName.Name = "tbEmployeeName";
+            this.tbEmployeeName.Size = new System.Drawing.Size(134, 20);
+            this.tbEmployeeName.TabIndex = 20;
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(24, 135);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(42, 13);
+            this.lblAdress.TabIndex = 19;
+            this.lblAdress.Text = "Adress:";
+            // 
             // CreateAccountForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(284, 314);
+            this.Controls.Add(this.lblRights);
+            this.Controls.Add(this.cbRights);
+            this.Controls.Add(this.lbPhoneNumber);
+            this.Controls.Add(this.lblEmployeeName);
+            this.Controls.Add(this.tbPhoneNumber);
+            this.Controls.Add(this.tbAddress);
+            this.Controls.Add(this.tbEmployeeName);
+            this.Controls.Add(this.lblAdress);
             this.Controls.Add(this.btnCreateAccount);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.lblRepeatPassword);
@@ -139,5 +220,13 @@
         private System.Windows.Forms.TextBox tbRepeatPassword;
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Label lblRights;
+        private System.Windows.Forms.ComboBox cbRights;
+        private System.Windows.Forms.Label lbPhoneNumber;
+        private System.Windows.Forms.Label lblEmployeeName;
+        private System.Windows.Forms.TextBox tbPhoneNumber;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.TextBox tbEmployeeName;
+        private System.Windows.Forms.Label lblAdress;
     }
 }
